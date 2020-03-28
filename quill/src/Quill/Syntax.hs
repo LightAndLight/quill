87e2b362aa80fc5ddb7403b70b79a51016b87574
Isaac Elliott <isaace71295@gmail.com>
2020-03-28 06:33:30 +0000

    introduced a notion of subtyping, with evidence

--- quill/src/Quill/Syntax.hs
+++ quill/src/Quill/Syntax.hs
@@ -7,16 +7,22 @@ module Quill.Syntax
   , recordPunned
   , Decl(..)
   , TableItem(..)
+  , bisubstExpr
+  , bisubstQuery
   )
 where
 
 import Prelude hiding (Ordering(..))
 
-import Bound.Class (Bound(..))
-import Bound (Scope)
+import Bound (Scope(..))
+import Bound.Scope (bitraverseScope)
 import Bound.Var (Var(..), unvar)
 import Control.Monad (ap)
+import Data.Bifunctor (Bifunctor(..))
+import Data.Bifoldable (Bifoldable(..))
+import Data.Bitraversable (Bitraversable(..), bimapDefault, bifoldMapDefault)
 import Data.Text (Text)
+import Data.Traversable (fmapDefault, foldMapDefault)
 import Data.Vector (Vector)
 import Data.Void (Void)
 
@@ -40,62 +46,163 @@ data TableItem
   = Field Text Type
   | Constraint Text (Vector Text)
 
-data Query f a
+data Query a b
   = SelectFrom Text
-  | InsertInto (f a) Text
-  | InsertIntoReturning (f a) Text
-  | Bind (Query f a) Text (Query f (Var () a))
-  | Return (f a)
-  deriving (Functor, Foldable, Traversable)
-instance Bound Query where
-  a >>>= f =
-    case a of
-      SelectFrom n -> SelectFrom n
-      InsertInto b c -> InsertInto (b >>= f) c
-      InsertIntoReturning b c -> InsertIntoReturning (b >>= f) c
-      Bind b c d ->
-        Bind (b >>>= f) c (d >>>= unvar (pure . B) (fmap F . f))
-      Return b -> Return (b >>= f)
+  | InsertInto (Expr b a) Text
+  | InsertIntoReturning (Expr b a) Text
+  | Bind (Query a b) Text (Scope () (Query a) b)
+  | Return (Expr b a)
+  | QVar b
+  | QName Text
+instance Functor (Query a) where; fmap = fmapDefault
+instance Foldable (Query a) where; foldMap = foldMapDefault
+instance Traversable (Query a) where; traverse = bitraverse pure
+instance Bifunctor Query where; bimap = bimapDefault
+instance Bifoldable Query where; bifoldMap = bifoldMapDefault
+instance Bitraversable Query where
+  bitraverse f g query =
+    case query of
+      SelectFrom a -> pure $ SelectFrom a
+      InsertInto a b -> (\a' -> InsertInto a' b) <$> bitraverse g f a
+      InsertIntoReturning a b -> (\a' -> InsertIntoReturning a' b) <$> bitraverse g f a
+      Bind a b c -> (\a' c' -> Bind a' b c') <$> bitraverse f g a <*> bitraverseScope f g c
+      Return a -> Return <$> bitraverse g f a
+      QVar a -> QVar <$> g a
+      QName a -> pure $ QName a
+instance Applicative (Query a) where; pure = return; (<*>) = ap
+instance Monad (Query a) where
+  return = QVar
+  (>>=) = flip (bisubstQuery pure)
 
-data Expr a
+bisubstScopeQuery :: (a -> Expr d c) -> (b -> Query c d) -> Scope () (Query a) b -> Scope () (Query c) d
+bisubstScopeQuery f g =
+  Scope .
+  bisubstQuery
+    (first (F . QVar) . f)
+    (unvar (QVar . B) (bisubstQuery (first (F . QVar) . f) (QVar . F . g))) .
+  unscope
+
+bisubstQuery :: (a -> Expr d c) -> (b -> Query c d) -> Query a b -> Query c d
+bisubstQuery f g query =
+    case query of
+      QName a -> QName a
+      QVar a -> g a
+      SelectFrom n -> SelectFrom n
+      InsertInto b c -> InsertInto (bisubstExpr g f b) c
+      InsertIntoReturning b c -> InsertIntoReturning (bisubstExpr g f b) c
+      Bind b c d ->
+        Bind (bisubstQuery f g b) c (bisubstScopeQuery f g d)
+      Return b -> Return (bisubstExpr g f b)
+
+data Expr a b
   = For
       Text
-      (Expr a) -- values
-      (Maybe (Scope () Expr a)) -- predicate
-      (Scope () Expr a) -- yield
+      (Expr a b) -- values
+      (Maybe (Scope () (Expr a) b)) -- predicate
+      (Scope () (Expr a) b) -- yield
   | Name Text
-  | Project (Expr a) Text
-  | Var a
-  | Record (Vector (Text, Expr a))
+  | Var b
+
+  | Record (Vector (Text, Expr a b))
+  | Project (Expr a b) Text
+  | Extend Text (Expr a b) (Expr a b)
+  | Update Text (Text, Scope () (Expr a) b) (Expr a b)
+
   | Int Int
   | Bool Bool
-  | Many (Vector (Expr a))
+  | Many (Vector (Expr a b))
 
-  | AND (Expr a) (Expr a)
-  | OR (Expr a) (Expr a)
-  | EQ (Expr a) (Expr a)
-  | NOT (Expr a)
-  deriving (Functor, Foldable, Traversable)
-instance Applicative Expr where; pure = return; (<*>) = ap
-instance Monad Expr where
+  | Some (Expr a b)
+  | None
+  | FoldOptional (Expr a b) (Text, Scope () (Expr a) b) (Expr a b)
+
+  | AND (Expr a b) (Expr a b)
+  | OR (Expr a b) (Expr a b)
+  | EQ (Expr a b) (Expr a b)
+  | NOT (Expr a b)
+
+  | Embed (Query b a)
+instance Functor (Expr a) where; fmap = fmapDefault
+instance Foldable (Expr a) where; foldMap = foldMapDefault
+instance Traversable (Expr a) where; traverse = bitraverse pure
+instance Bifunctor Expr where; bimap = bimapDefault
+instance Bifoldable Expr where; bifoldMap = bifoldMapDefault
+instance Bitraversable Expr where
+  bitraverse f g e =
+    case e of
+      For a b c d ->
+        For a <$>
+        bitraverse f g b <*>
+        traverse (bitraverseScope f g) c <*>
+        bitraverseScope f g d
+      Name a -> pure $ Name a
+      Var b -> Var <$> g b
+      Record a -> Record <$> (traverse.traverse) (bitraverse f g) a
+      Project a b -> (\a' -> Project a' b) <$> bitraverse f g a
+      Extend a b c -> Extend a <$> bitraverse f g b <*> bitraverse f g c
+      Update a (n, b) c -> (\b' -> Update a (n, b')) <$> bitraverseScope f g b <*> bitraverse f g c
+      Int a -> pure $ Int a
+      Bool a -> pure $ Bool a
+      Many a -> Many <$> traverse (bitraverse f g) a
+      Some a -> Some <$> bitraverse f g a
+      None -> pure None
+      FoldOptional a (n, b) c ->
+        (\a' b' -> FoldOptional a' (n, b')) <$>
+        bitraverse f g a <*>
+        bitraverseScope f g b <*>
+        bitraverse f g c
+      AND a b -> AND <$> bitraverse f g a <*> bitraverse f g b
+      OR a b -> OR <$> bitraverse f g a <*> bitraverse f g b
+      EQ a b -> EQ <$> bitraverse f g a <*> bitraverse f g b
+      NOT a -> NOT <$> bitraverse f g a
+      Embed a -> Embed <$> bitraverse g f a
+instance Applicative (Expr a) where; pure = return; (<*>) = ap
+instance Monad (Expr a) where
   return = Var
-  m >>= f =
-    case m of
-      For n value cond yield ->
-        For n (value >>= f) (fmap (>>>= f) cond) (yield >>>= f)
-      Name n -> Name n
-      Project value field -> Project (value >>= f) field
-      Var n -> f n
-      Record fields -> Record $ (fmap.fmap) (>>= f) fields
-      Int n -> Int n
-      Bool b -> Bool b
-      Many values -> Many $ (>>= f) <$> values
-      AND a b -> AND (a >>= f) (b >>= f)
-      OR a b -> OR (a >>= f) (b >>= f)
-      EQ a b -> EQ (a >>= f) (b >>= f)
-      NOT a -> NOT (a >>= f)
+  (>>=) = flip (bisubstExpr pure)
 
-recordPunned :: (a -> Text) -> Vector a -> Expr a
+bisubstScopeExpr :: (a -> Query d c) -> (b -> Expr c d) -> Scope () (Expr a) b -> Scope () (Expr c) d
+bisubstScopeExpr f g =
+  Scope .
+  bisubstExpr
+    (first (F . Var) . f)
+    (unvar (Var . B) (bisubstExpr (first (F . Var) . f) (Var . F . g))) .
+  unscope
+
+bisubstExpr :: (a -> Query d c) -> (b -> Expr c d) -> Expr a b -> Expr c d
+bisubstExpr f g e =
+  case e of
+    For n value cond yield ->
+      For n (bisubstExpr f g value) (bisubstScopeExpr f g <$> cond) (bisubstScopeExpr f g yield)
+    Name n -> Name n
+    Var n -> g n
+
+    Record fields -> Record $ (fmap.fmap) (bisubstExpr f g) fields
+    Project value field -> Project (bisubstExpr f g value) field
+    Extend field value rest -> Extend field (bisubstExpr f g value) (bisubstExpr f g rest)
+    Update field (n, func) rest ->
+      Update
+        field
+        (n, bisubstScopeExpr f g func)
+        (bisubstExpr f g rest)
+
+    Int n -> Int n
+    Bool b -> Bool b
+    Many values -> Many $ bisubstExpr f g <$> values
+
+    Some a -> Some (bisubstExpr f g a)
+    None -> None
+    FoldOptional a (n, b) c ->
+      FoldOptional (bisubstExpr f g a) (n, bisubstScopeExpr f g b) (bisubstExpr f g c)
+
+    AND a b -> AND (bisubstExpr f g a) (bisubstExpr f g b)
+    OR a b -> OR (bisubstExpr f g a) (bisubstExpr f g b)
+    EQ a b -> EQ (bisubstExpr f g a) (bisubstExpr f g b)
+    NOT a -> NOT (bisubstExpr f g a)
+
+    Embed a -> Embed (bisubstQuery g f a)
+
+recordPunned :: (b -> Text) -> Vector b -> Expr a b
 recordPunned f fields = Record $ (\field -> (f field, Var field)) <$> fields
 
 data Decl
@@ -105,9 +212,9 @@ data Decl
       Text
       (Vector (Text, Type))
       Type
-      (Query Expr (Var Int Void))
+      (Query (Var Int Void) Void)
   | Function
       Text
       (Vector (Text, Type))
       Type
-      (Scope Int Expr Void)
+      (Scope Int (Expr Void) Void)
