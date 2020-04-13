4c1e284c76624388708c1ad1a87a71bfc91621c2
Isaac Elliott <isaace71295@gmail.com>
2020-04-13 06:31:16 +0000

    Implemented checkMigration

--- quill/src/Quill/Check/Migration.hs
+++ quill/src/Quill/Check/Migration.hs
@@ -1,6 +1,8 @@
 {-# language DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 {-# language FlexibleContexts #-}
 {-# language LambdaCase #-}
+{-# language ScopedTypeVariables #-}
+{-# language ViewPatterns #-}
 module Quill.Check.Migration
   ( MigrationError(..)
   , MigrationEnv
@@ -11,21 +13,24 @@ where
 
 import Control.Monad (when)
 import Control.Monad.Except (MonadError, throwError)
-import Data.Foldable (foldlM)
+import Data.Foldable (foldl', foldlM)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Text (Text)
 import Data.Traversable (for)
 import Data.Vector (Vector)
+import qualified Data.Vector as Vector
 
-import Quill.Syntax (Constraint, Type)
+import Quill.Check (ConstraintError, checkConstraint, mapError)
+import Quill.Syntax (Constraint(..), Type)
 import qualified Quill.Syntax as Syntax
-import Quill.Syntax.Migration (Command(..), Migration(..))
+import Quill.Syntax.Migration (Command(..), FieldChange(..), Migration(..))
 import qualified Quill.Syntax.Migration as Migration (Name(..))
 
-data Tracked a = Tracked { _origin :: Migration.Name, _data :: a }
+data Tracked a = Tracked { origin :: Migration.Name, unTracked :: a }
   deriving (Functor, Foldable, Traversable)
 
 data TableItem
@@ -51,14 +56,19 @@ data MigrationError
   = DuplicateMigration Migration.Name
   | TableAlreadyExists Text
   | TableDoesn'tExist Text
+  | FieldNotFound Text
+  | FieldAlreadyExists Text
+  | ConstraintError (ConstraintError ())
+  deriving Show
 
 checkCommand ::
+  forall m.
   MonadError MigrationError m =>
   Migration.Name ->
   MigrationEnv ->
   Command ->
   m MigrationEnv
-checkCommand m env c =
+checkCommand currentMigration env c =
   case c of
     CreateTable tableName fields -> do
       when (Map.member tableName $ _meTables env) . throwError $ TableAlreadyExists tableName
@@ -66,23 +76,94 @@ checkCommand m env c =
         table :: Table
         table =
           Table $
-          Tracked m .
+          track .
           (\case
              Syntax.Field fieldName fieldType ->
-               Field fieldName (Tracked m fieldType)
+               Field fieldName (track fieldType)
              Syntax.Constraint constr args ->
                Constraint constr args
           ) <$>
           fields
-      pure $ env { _meTables = Map.insert tableName (Tracked m table) $ _meTables env }
+      pure $ env { _meTables = Map.insert tableName (track table) $ _meTables env }
     AlterTable tableName changes -> do
       entry <- maybe (throwError $ TableDoesn'tExist tableName) pure $ Map.lookup tableName (_meTables env)
-      entry' <-
-        for entry $ \entry' -> do
-          let items = unTable entry'
-          items' <- _ items
-          pure $ Table items'
+      entry' <- for entry $ \entry' -> foldlM (runChange tableName) entry' changes
       pure $ env { _meTables = Map.insert tableName entry' $ _meTables env }
+  where
+    track x = Tracked { origin = currentMigration, unTracked = x }
+
+    dropField ::
+      Table ->
+      Text ->
+      m Table
+    dropField table fieldName =
+      let
+        cond = (\case; Field n _ -> n == fieldName; _ -> False) . unTracked
+      in
+        case Vector.find cond (unTable table) of
+          Nothing -> throwError $ FieldNotFound fieldName
+          Just{} -> pure . Table $ Vector.filter (not . cond) (unTable table)
+
+    addField ::
+      Table ->
+      Text ->
+      Type () ->
+      m Table
+    addField table fieldName fieldType =
+      let
+        cond = (\case; Field n _ -> n == fieldName; _ -> False) . unTracked
+      in
+        case Vector.find cond (unTable table) of
+          Just{} -> throwError $ FieldAlreadyExists fieldName
+          Nothing ->
+            pure . Table $
+            Vector.snoc
+              (unTable table)
+              (Tracked currentMigration $
+               Field fieldName (Tracked currentMigration fieldType)
+              )
+
+    addConstraint ::
+      Text ->
+      Table ->
+      Constraint ->
+      Vector Text ->
+      m Table
+    addConstraint tableName table constr args = do
+      mapError ConstraintError $
+        checkConstraint
+          tableName
+          (foldl'
+             (\acc item ->
+                case unTracked item of
+                  Constraint{} -> acc
+                  Field fieldName fieldType -> Map.insert fieldName (unTracked fieldType) acc
+             )
+             mempty
+             (unTable table)
+          )
+          (Maybe.isJust $
+           Vector.find
+             (\case; (unTracked -> Constraint PrimaryKey _) -> True; _ -> False)
+             (unTable table)
+          )
+          constr
+          args
+      pure . Table $
+        Vector.snoc
+          (unTable table)
+          (Tracked currentMigration $ Constraint constr args)
+
+    runChange ::
+      Text ->
+      Table ->
+      FieldChange ->
+      m Table
+    runChange tableName table change =
+      case change of
+        DropField fieldName -> dropField table fieldName
+        AddField fieldName fieldType -> addField table fieldName fieldType
+        AddConstraint constr args -> addConstraint tableName table constr args
 
 checkMigration ::
   MonadError MigrationError m =>
